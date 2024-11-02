86ef16c363bf1ca669083c8541595b176c36d3ad
williserdman <willis.erdman@gmail.com>
2024-11-02 16:36:46 -0400

    housekeeping

--- Python Code/Arduino Send.py
+++ Python Code/Arduino Send.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 
+
 def on_publish(client, userdata, mid, reason_code, properties):
     # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
     try:
@@ -11,18 +12,26 @@ def on_publish(client, userdata, mid, reason_code, properties):
         print("* publish() return the mid of the message sent.")
         print("* mid from publish() is added to unacked_publish by the main thread")
         print("* on_publish() is called by the loop_start thread")
-        print("While unlikely (because on_publish() will be called after a network round-trip),")
+        print(
+            "While unlikely (because on_publish() will be called after a network round-trip),"
+        )
         print(" this is a race-condition that COULD happen")
         print("")
-        print("The best solution to avoid race-condition is using the msg_info from publish()")
-        print("We could also try using a list of acknowledged mid rather than removing from pending list,")
+        print(
+            "The best solution to avoid race-condition is using the msg_info from publish()"
+        )
+        print(
+            "We could also try using a list of acknowledged mid rather than removing from pending list,"
+        )
         print("but remember that mid could be re-used !")
+
+
 unacked_publish = set()
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_publish = on_publish
 
 mqttc.user_data_set(unacked_publish)
-mqttc.connect("172.20.112.71", 1883, 60)
+mqttc.connect("172.20.183.196", 1883, 60)
 mqttc.loop_start()
 print("Connected")
 
