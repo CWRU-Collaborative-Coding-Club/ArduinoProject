953e9d7c2c016ffde782135ca946af24e7704539
luketierney <50178839+luketierney@users.noreply.github.com>
2024-11-22 16:03:57 -0500

    Added CORS to website recieve, allowing it to accept POST requests from other computers

--- Python Code/WebsiteRecieve.py
+++ Python Code/WebsiteRecieve.py
@@ -1,29 +1,30 @@
 from flask import Flask, request
 from markupsafe import escape
 from ArduinoSend import send
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+#@app.route("/")
+#def hello_world():
+#    return "<p>Hello, World!</p>"
 
 #   @app.route('/iot/<path:input>')
 #   def iot(input):
 #       send(f"{escape(input)}")
 #       return f"{escape(input)}"
     
-@app.route('/iot', methods=['POST'])
-def iotPost():
+@app.route('/', methods=['POST'])
+def iot():
     requestData = request.json
+    print(requestData)
     send(str(requestData))
     return requestData
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-    
-
+    app.run('0.0.0.0', debug=True, port=5000)
